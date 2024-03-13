d3bf9c351b59dc7bdc097b30d34ef9a032358e1f
ger <gerdavid7@gmail.com>
2024-03-13 12:23:23 +0300

    Initial commit

---
+++ src/test/java/com/ger/creswave/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ger.creswave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
